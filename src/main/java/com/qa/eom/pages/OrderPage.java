5fb2fa331d025a713a7299eb4647713fbe7391be
Ankita Gupta <ankita@Ankitas-MacBook-Air.local>
2022-12-15 20:24:09 +0530

    Added changes mentioned in PR

--- src/main/java/com/qa/eom/pages/OrderPage.java
+++ src/main/java/com/qa/eom/pages/OrderPage.java
@@ -1,9 +1,12 @@
 package com.qa.eom.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage 
 {
 String order;
-public void order()
+By orderNo = By.id("order-na");
+public void orderNumber()
 {
 	System.out.println("Order is :12345");
 }
